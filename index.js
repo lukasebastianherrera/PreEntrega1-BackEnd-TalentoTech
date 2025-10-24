3ac645acfebcaf4da73b297eaac3f04dfa469fbb
lukasebastianherrera <lukasebastianherrera2004@gmail.com>
2025-10-24 18:23:06 -0300

    add GET/

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const metodo = process.argv[2];
 const parametro = process.argv[3];
+
+// METODO GET
 if(metodo.toUpperCase()  == "GET" && parametro.toLowerCase() == "products"){
     fetch('https://fakestoreapi.com/products')
     .then(response => response.json())
     .then(data => console.log(data));
-} else if(metodo.toUpperCase()  === "POST"){
-    console.log(`Recibimos ${parametro} Satisfactoriamente`);
-} else if(metodo.toUpperCase()  == "PUT"){
+}
+// METODO GET/id
+if(metodo.toUpperCase()  === "GET" && parametro.startsWith("products/")){
+    const separador = parametro.split("/");
+    const [, id ] = separador;
+    fetch(`https://fakestoreapi.com/products/${id}`)
+    .then(response => response.json())
+    .then(data => console.log(data));
+
+} 
+if(metodo.toUpperCase()  == "PUT"){
     console.log(`Modificamos el item con id: ${parametro} satisfactoriamente`);
-} else if(metodo.toUpperCase()  == "DELETE"){
+} 
+if(metodo.toUpperCase()  == "DELETE"){
     console.log(`El item con el id: ${parametro} se eliminó con éxito`)
 }
 
-console.log("ss")
